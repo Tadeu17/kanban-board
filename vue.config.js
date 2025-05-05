79fcb775d57b4b8805bb39dc01abddfb00afcc1a
Tadeu <eng.tadeu.marques@gmail.com>
2025-05-05 17:27:44 +0100

    fix: HMR was not workin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   lintOnSave: false,
+  devServer: {
+    hot: true, // Ensure HMR is enabled
+    host: "localhost", // Explicitly set the host
+    port: 8080, // Set the port (default for Vue CLI)
+    client: {
+      overlay: true, // Show errors and warnings in the browser overlay
+    },
+  },
 });
